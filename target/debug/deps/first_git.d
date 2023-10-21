1a62d5eb613bd7052c66dabab3a62d0ae2b9869c
FoxyLang <ebkr00@gmail.com>
2023-10-21 16:59:33 -0700

    First edit

---
+++ target/debug/deps/first_git.d
@@ -0,0 +1,5 @@
+C:\Users\ebkr0\RustPrograms\Projects\first_git\target\debug\deps\first_git.exe: src\main.rs
+
+C:\Users\ebkr0\RustPrograms\Projects\first_git\target\debug\deps\first_git.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/first_git.d
@@ -0,0 +1,1 @@
+C:\Users\ebkr0\RustPrograms\Projects\first_git\target\debug\first_git.exe: C:\Users\ebkr0\RustPrograms\Projects\first_git\src\main.rs
